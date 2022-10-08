26ed5a6d7e1916d1cfeb55a54414870be6fd338a
Sandeep <Sandeep@NeeOm.lan>
2022-10-08 18:28:09 +0400

    This is my first commit

---
+++ src/practice/FirtClass.java
@@ -0,0 +1,10 @@
+package practice;
+
+public class FirtClass {
+	
+		public static void main(String[] args) {
+	        System.out.println("Hello, World!"); 
+	        System.out.println("My First Java Program");
+	    }
+
+}
